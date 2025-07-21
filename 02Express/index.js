345c3e9f9e443370a5fc8338768b2c135e46e219
Ritesh Kumar <rav332211@gmail.com>
2025-07-21 08:25:57 +0530

    push the data

--- 02Express/index.js
+++ 02Express/index.js
@@ -1,46 +1,51 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json");
-
+const fs = require("fs");
 
 const app = express();
-const port=8000;
+const port = 8000;
 
-app.get("/users",(req,res)=>{
-    const html = `
-    <ul>
-        ${users.map((user)=>`<li>${user.first_name}</li>.join(" ")`)}
-    </ul>
-    `
-    res.send(html);
-})
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/api/users',(req,res)=>{    
-    return res.json(users);
+app.get("/users", (req, res) => {
+  const html = `
+    <ul>${users.map((user) => `<li>${user.first_name}</li>`).join(" ")}</ul>
+    `;
+  res.send(html);
 });
 
-app.get('/api/users/:id',(req,res)=>{    
-   const id = Number(req.params.id);
-   const user = users.find((user)=>user.id === id);
-    return res.json(user);
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  return res.json(user);
 });
 
 // app.route("api/users/:id").get((req,res)=>{
 
 // })
 
-app.post('api/users',(req,res)=>{
-    //todo : create new users
-    return res.json({status:pending});
+app.post("/api/users", (req, res) => {
+  const body = req.body;
+  users.push({...body, id: users.length+1});
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "Success", id: users.length});
+  });
 });
 
-app.patch('api/users:id',(req,res)=>{
-    //todo : edit the user with id 
-    return res.json({status:pending});
+app.patch("/api/users:id", (req, res) => {
+  //todo : edit the user with id
+  return res.json({ status: "pending", });
 });
 
-app.delete('api/users:id',(req,res)=>{
-    //todo : edit the user with id 
-    return res.json({status:pending});
+app.delete("/api/users:id", (req, res) => {
+  //todo : edit the user with id
+  return res.json({ status: "pending" });
 });
 
-app.listen(port,()=>console.log(`server started at port http://localhost:${port}`))
+app.listen(port, () =>
+  console.log(`server started at port http://localhost:${port}`)
+);
